dc42ecc0ee7c16d1322b94be017c2d5059d963fe
Steven Farrell <stevenfarrelltraining@yahoo.com>
2019-09-24 21:54:20 -0400

    Got it to print a string, have to finish it up to print the actual number in expanded form

--- expandTheNumber.playground/Contents.swift
+++ expandTheNumber.playground/Contents.swift
@@ -1,7 +1,26 @@
 import Foundation
 
+var thousandString = Int()
+var hundredString = Int()
+var tensString = Int()
+var singleString = Int()
+var defaultString = "Give me a number to expand"
+
 func expandTheNumber(_ number: Int) -> [Int] {
-    
+    if number > 1000 {
+        print("\(thousandString), \(hundredString), \(tensString), \(singleString)")
+    } else if number < 1000 {
+        print("\(hundredString), \(tensString), \(singleString)")
+    } else if number < 100 {
+        print("\(tensString), \(singleString)")
+    } else if number < 10 {
+        print("\(singleString)")
+    } else if number <= 0 {
+        print("0")
+    } else {
+        print(defaultString)
+}
+    return[]
 }
 
 expandTheNumber(199)  // [100, 90, 9]
@@ -10,3 +29,4 @@ expandTheNumber(0)    // [0]
 expandTheNumber(562)  // [500, 60, 2]
 expandTheNumber(5280) // [5000, 200, 80, 0]
 expandTheNumber(560)  // [500, 60, 0]
+
